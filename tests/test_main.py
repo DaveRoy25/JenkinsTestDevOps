ca5650bf2956a028a095c72cf31a7f7f0011d5e8
DaveRoy25 <dave.roytberg@gmail.com>
2025-01-03 10:53:39 +0200

    add print statement to test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,10 +3,12 @@ from main import add, subtract
 
 class TestMain(unittest.TestCase):
     def test_add(self):
+        print("Running test_add()...")  # This will print in Jenkins console
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
     
     def test_subtract(self):
+        print("Running test_subtract()...")  # This will also print
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(10, 15), -5)
 
